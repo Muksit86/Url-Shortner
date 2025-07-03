9b6cbf58d8672fff09587abcd8a397422fac91f9
Muksit86 <amuksit7@gmail.com>
2025-07-03 19:06:54 +0530

    changes in db.js

--- Backend/Controllers/db.js
+++ Backend/Controllers/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const dbConnect = async () =>{
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
         console.log("MongoDB connected")
     } catch (error) {
          console.error(error)
